4a93a5b37048a588c407fd52bc9d7a4d8c9408cf
DanielKuhnDeAraujo <danielaprendendo00@gmail.com>
2026-02-27 11:06:02 -0300

    criando o repo

---
+++ teste.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h2>Variáveis e constantes PHP</h2>
+    <?php
+        echo "<p>Hello World!</p>";
+        // Comentário
+        /* Comentário
+        de muitas 
+        linhas*/
+        /*0
+        echo $_SERVER['HTTP_USER_AGENT'];
+        if (str_contains($_SERVER['HTTP_USER_AGENT'], 'Firefox')) {
+            echo 'Você está usando o Firefox.';
+        }*/
+        $a = 2;//int
+        $b = 0.0;//float
+        $c = "xablau";//string
+        $d = 'xablau 2';//string
+        $e = null;//nulo
+        ?>
+</body>
+
+</html>
